1a57fd32a6ae0577f085ff6f91cf4393793bc5c2
rickylobo626 <loboman96@gmail.com>
2022-05-15 03:29:08 +0100

    Add comments

--- script.js
+++ script.js
@@ -4,11 +4,13 @@ let playerPick, computerPick;
 let playerScore;
 let computerScore;
 
+// Random computer play
 function computerPlay() {
   let playArray = ['rock', 'paper', 'scissors'];
   return playArray[Math.floor(Math.random() * 3)];
 }
 
+// Play round
 function playRound(playerSelection, computerSelection) {
   let lowerStr = playerSelection.toLowerCase();
 
@@ -18,37 +20,41 @@ function playRound(playerSelection, computerSelection) {
   } else if (isTie(lowerStr, computerSelection)) {
     return `It's a tie. The computer also chose ${computerSelection}`;
   }
-    computerScore++;
-    return `You lose the round! The computer chose ${computerSelection}.`;
+  computerScore++;
+  return `You lose the round! The computer chose ${computerSelection}.`;
   }
 
+// Check if player wins the round
 function playerWinsRound(playerSelection, computerSelection) {
-if (
-  playerSelection == 'rock' && computerSelection == 'scissors' ||
-  playerSelection == 'paper' && computerSelection == 'rock' ||
-  playerSelection == 'scissors' && computerSelection == 'paper'
-) {
-  return true;
-}
+  if (
+    playerSelection == 'rock' && computerSelection == 'scissors' ||
+    playerSelection == 'paper' && computerSelection == 'rock' ||
+    playerSelection == 'scissors' && computerSelection == 'paper'
+  ) {
+    return true;
+  }
   return false;
 }
 
+// Check if it's a Tie
 function isTie(playerSelection, computerSelection) {
-if (playerSelection == computerSelection) {
-  return true;
-}
+  if (playerSelection == computerSelection) {
+    return true;
+  }
   return false;
 }
 
+// Announce the Winner
 function checkWinner() {
   if (playerScore > computerScore) {
     return 'YOU WIN!';
   } else if (computerScore > playerScore) {
     return 'YOU LOSE!';
   }
-    return 'DRAW!';
+  return 'DRAW!';
 }
 
+// Start game
 function game() {
   playerScore = 0;
   computerScore = 0;
