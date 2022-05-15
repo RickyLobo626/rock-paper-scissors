c2c085034d4b8ce6f247ba7581458b0f8c7de104
rickylobo626 <loboman96@gmail.com>
2022-05-15 03:10:59 +0100

    Add scores and a function that checks who won the game

--- script.js
+++ script.js
@@ -1,47 +1,65 @@
 'use strict';
 
 let playerPick, computerPick;
+let playerScore;
+let computerScore;
 
 function computerPlay() {
-let playArray = ['rock', 'paper', 'scissors'];
-return playArray[Math.floor(Math.random() * 3)];
+  let playArray = ['rock', 'paper', 'scissors'];
+  return playArray[Math.floor(Math.random() * 3)];
 }
 
 function playRound(playerSelection, computerSelection) {
-let lowerStr = playerSelection.toLowerCase();
+  let lowerStr = playerSelection.toLowerCase();
 
-if (playerWinsRound(lowerStr, computerSelection)) {
-return `You won this round! The computer chose ${computerSelection}.`;
-} else if (isTie(lowerStr, computerSelection)) {
-return `It's a tie. The computer also chose ${computerSelection}`;
-}
-return `You lose the round! The computer chose ${computerSelection}.`;
-}
+  if (playerWinsRound(lowerStr, computerSelection)) {
+    playerScore++;
+    return `You won this round! The computer chose ${computerSelection}.`;
+  } else if (isTie(lowerStr, computerSelection)) {
+    return `It's a tie. The computer also chose ${computerSelection}`;
+  }
+    computerScore++;
+    return `You lose the round! The computer chose ${computerSelection}.`;
+  }
 
 function playerWinsRound(playerSelection, computerSelection) {
 if (
-playerSelection == 'rock' && computerSelection == 'scissors' ||
-playerSelection == 'paper' && computerSelection == 'rock' ||
-playerSelection == 'scissors' && computerSelection == 'paper'
+  playerSelection == 'rock' && computerSelection == 'scissors' ||
+  playerSelection == 'paper' && computerSelection == 'rock' ||
+  playerSelection == 'scissors' && computerSelection == 'paper'
 ) {
-return true;
+  return true;
 }
-return false;
+  return false;
 }
 
 function isTie(playerSelection, computerSelection) {
 if (playerSelection == computerSelection) {
-return true;
+  return true;
 }
-return false;
+  return false;
+}
+
+function checkWinner() {
+  if (playerScore > computerScore) {
+    return 'YOU WIN!';
+  } else if (computerScore > playerScore) {
+    return 'YOU LOSE!';
+  }
+    return 'DRAW!';
 }
 
 function game() {
-for (let i = 0; i < 5; i++) {
-playerPick = prompt('Rock, paper or scissors?');
-computerPick = computerPlay();
-console.log(playRound(playerPick, computerPick));
-}
+  playerScore = 0;
+  computerScore = 0;
+  for (let i = 0; i < 5; i++) {
+    playerPick = prompt('Rock, paper or scissors?');
+    computerPick = computerPlay();
+    console.log(playRound(playerPick, computerPick));
+    console.log(`Your Score: ${playerScore} | Computer Score: ${computerScore}`);
+  }
+  return checkWinner();
 }
 
-game();
+console.log(game());
+
