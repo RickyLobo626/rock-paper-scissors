1a96114a3a04a4e780a9f18f4a07314222ab309f
rickylobo626 <loboman96@gmail.com>
2022-06-18 19:28:11 +0100

    Improve UI and add restart button

--- script.js
+++ script.js
@@ -3,57 +3,69 @@
 const playerBtn = document.querySelectorAll('#player-div > div');
 const comment = document.getElementById('comment');
 const enemyImg = document.getElementById('enemy-img');
-const enemyImgDiv = document.querySelector('#enemy');
+const enemyDiv = document.querySelector('#enemy');
 const enemyComment = document.querySelector('#enemy-comment');
-const playerScore = document.querySelector('.score');
-const enemyScore = document.querySelector('.enemy-score');
+const scoreSpan = document.querySelector('.score');
+const enemyScoreSpan = document.querySelector('.enemy-score');
 const result = document.querySelector('#result');
+const restartBtn = document.getElementById('restart');
 let playerPick, computerPick;
 let score = 0;
 let computerScore = 0;
 let round = 0;
+let playing = true;
 
 playerBtn.forEach(btn => {
   btn.addEventListener('click', () => {
-    playerPick = btn.className;
-    computerPick = computerPlay();
+    
+    if (playing) {
+      playerPick = btn.className;
+      computerPick = computerPlay();
 
-    // Show enemy
-    result.classList.add('hidden');
-    enemyImgDiv.classList.remove('hidden');
-    enemyImg.classList.remove('hidden');
-    enemyComment.classList.remove('hidden');
+      // Show enemy
+      enemyImg.classList.remove('hidden');
 
-    // Add and remove classes
-    if (enemyImgDiv.classList.contains(enemyImgDiv.className)) {
-      enemyImgDiv.classList.remove(enemyImgDiv.className);
-      enemyImgDiv.classList.add(computerPick);
-    } else {
-      enemyImgDiv.classList.add(computerPick);
-    }
+      // Add and remove classes
+      if (enemyDiv.classList.contains(enemyDiv.className)) {
+        enemyDiv.classList.remove(enemyDiv.className);
+        enemyDiv.classList.add(computerPick);
+      } else {
+        enemyDiv.classList.add(computerPick);
+      }
 
-    enemyImg.src = `./images/enemy-${computerPick}.png`;
-    enemyComment.textContent = `The enemy chose ${computerPick}`;
-    playRound();
-    round++;
-    if (round === 5) {
-      checkWinner();
-      resetGame();
-      result.classList.remove('hidden');
+      playRound();
+      enemyImg.src = `./images/enemy-${computerPick}.png`;
+      enemyComment.textContent = `The enemy chose ${computerPick}`;
+      scoreSpan.textContent = score;
+      enemyScoreSpan.textContent = computerScore;
+      round++;
+
+      // If it's the final round check winner and stop game
+      if (round === 5) {
+        playing = false;
+        checkWinner();
+        restartBtn.classList.remove('hidden');
+      }
     }
   });
 });
 
+restartBtn.addEventListener('click', resetGame);
+
+// Reset game
 function resetGame() {
+  playing = true;
   score = 0;
   computerScore = 0;
   round = 0;
-  playerScore.textContent = 0;
-  enemyScore.textContent = 0;
+  scoreSpan.textContent = 0;
+  enemyScoreSpan.textContent = 0;
+  enemyComment.textContent = '';
+  result.textContent = '';
+  enemyImg.classList.add('hidden');
+  restartBtn.classList.add('hidden');
   comment.textContent = 'Choose one to play again!'
-  enemyComment.classList.add('hidden');
 }
-
 
 // Random computer play
 function computerPlay() {
@@ -65,15 +77,13 @@ function computerPlay() {
 function playRound() {
   if (playerWinsRound()) {
     score++;
-    playerScore.textContent = score;
     comment.textContent = `You chose ${playerPick}. You win this round!`;
     return;
   } else if (isTie()) {
-    comment.textContent = `You chose ${playerPick}. It's a tie.`;
+    comment.textContent = `You chose ${playerPick}. It's a tie!`;
     return;
   }
   computerScore++;
-  enemyScore.textContent = computerScore;
   comment.textContent = `You chose ${playerPick}. You lose this round!`;
   return;
 }
@@ -99,15 +109,15 @@ function isTie() {
 // Announce the Winner
 function checkWinner() {
   if (score > computerScore) {
-    result.textContent = 'YOU WIN!';
+    result.textContent = 'YOU WIN';
     result.style['color'] = '#1a8a1a';
     return;
   } else if (computerScore > score) {
-    result.textContent = 'YOU LOSE!';
+    result.textContent = 'YOU LOSE';
     result.style['color'] = '#c93030';
     return;
   }
-  result.textContent = 'DRAW!';
+  result.textContent = 'DRAW';
   result.style['color'] = '#3785df';
   return;
 }
