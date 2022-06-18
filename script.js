82206482c4d576a4b8f37e1690aa32cf35931921
rickylobo626 <loboman96@gmail.com>
2022-06-18 22:17:33 +0100

    Improve UI so it's easy to understand when the game ends

--- script.js
+++ script.js
@@ -9,6 +9,7 @@ const scoreSpan = document.querySelector('.score');
 const enemyScoreSpan = document.querySelector('.enemy-score');
 const result = document.querySelector('#result');
 const restartBtn = document.getElementById('restart');
+const postGameDiv = document.getElementById('post-game');
 let playerPick, computerPick;
 let score = 0;
 let computerScore = 0;
@@ -45,6 +46,7 @@ playerBtn.forEach(btn => {
         playing = false;
         checkWinner();
         restartBtn.classList.remove('hidden');
+        postGameDiv.classList.remove('hidden');
       }
     }
   });
@@ -62,6 +64,7 @@ function resetGame() {
   enemyScoreSpan.textContent = 0;
   enemyComment.textContent = '';
   result.textContent = '';
+  postGameDiv.classList.add('hidden');
   enemyImg.classList.add('hidden');
   restartBtn.classList.add('hidden');
   comment.textContent = 'Choose one to play again!'
@@ -111,13 +114,16 @@ function checkWinner() {
   if (score > computerScore) {
     result.textContent = 'YOU WIN';
     result.style['color'] = '#1a8a1a';
+    result.style['text-shadow'] = '0 0 10px #b8f39d'
     return;
   } else if (computerScore > score) {
     result.textContent = 'YOU LOSE';
     result.style['color'] = '#c93030';
+    result.style['text-shadow'] = '0 0 10px #ff6b6b'
     return;
   }
   result.textContent = 'DRAW';
   result.style['color'] = '#3785df';
+  result.style['text-shadow'] = '0 0 10px #58cfff'
   return;
 }
